36e99afd1aee90a8c77c12a15a626bfe9f718608
Agnes Sliczniak <aga@admins-Air.local>
2020-06-28 13:48:05 -0500

    more files

--- data/idph/cron.sh
+++ data/idph/cron.sh
@@ -4,6 +4,9 @@
 #http://dph.illinois.gov/sites/default/files/Illinois_County_Geo_ch.json
 #https://www.dph.illinois.gov/sites/default/files/
 # IL_hospital_regions_map.geojson?nocache=1
+#http://www.dph.illinois.gov/sitefiles/CountyList.json?nocache=1
+#http://www.dph.illinois.gov/sites/default/files/
+# Illinois_County_Geo_ch.json?nocache=true
 
 o=`/bin/date -j '+%Y %m %d %H:%M:%S-%Z'` || exit
 set -- $o
@@ -13,18 +16,20 @@ n=''
 foo() {
 	f=$1$o; shift
 	n="$n $f"
-	u="$*"
+	u="$h""$*"'.json?nocache=1'
 }
 
-h='https://dph.illinois.gov/sitefiles/COVIDHospitalRegions.json?nocache=1'
-u='https://dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1'
-for u in hosp\ $h \
-  zip\ https://dph.illinois.gov/sitefiles/COVIDZip.json?nocache=1 \
-  demo\ https://dph.illinois.gov/sitefiles/CountyDemos.json?nocache=1 \
-  tests\ $u; do
+h='https://dph.illinois.gov/sitefiles/'
+for u in hosp\ COVIDHospitalRegions zip\ COVIDZip demo\ CountyDemos \
+  risk\ COVIDCountyRiskMetrics synd\ COVIDSyndromic \
+  tests\ COVIDHistoricalTestResults; do
 	foo $u
 	curl -sqL -- "$u" | /usr/bin/tr -d \\r >"$f"
+	#echo "$u" "$f"
 done
+
+#echo "$n"
+#exit
 
 /usr/bin/file -- $n
 [ -t 0 ] && echo "add *$o" || git add -v -- $n
